9853bbb88d38888ec23f2c02d94c76fc16a68bf0
Bruno de Souza <130676240+psibrunosg@users.noreply.github.com>
2026-08-14 10:23:07 -0300

    redesign: config base (tailwind, scripts, workflow, entry)

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,2 @@
-const CACHE = 'bsnutri-shell-v2'
-const APP_SHELL = ['/bsnutri/', '/bsnutri/manifest.webmanifest', '/bsnutri/app-icon.png']
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)))
-  self.skipWaiting()
-})
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  )
-})
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return
-  event.respondWith(fetch(event.request).then(response => {
-    if (response.ok && new URL(event.request.url).origin === self.location.origin) {
-      const copy = response.clone()
-      caches.open(CACHE).then(cache => cache.put(event.request, copy))
-    }
-    return response
-  }).catch(() => caches.match(event.request).then(cached => cached ?? caches.match('/bsnutri/'))))
-})
+self.addEventListener('install', () => self.skipWaiting())
+self.addEventListener('activate', (e) => e.waitUntil(self.registration.unregister()))
